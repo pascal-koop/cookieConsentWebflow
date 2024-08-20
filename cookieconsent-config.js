d5b872ff3cad807fa47b248befb35dd0d2be2af3
Pascal <pascale.koop@live.com>
2024-08-20 10:50:48 +0200

    Delete youtube

--- cookieconsent-config.js
+++ cookieconsent-config.js
@@ -4,7 +4,6 @@ import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookiecon
  * https://cookieconsent.orestbida.com/reference/configuration-reference.html
  */
 CookieConsent.run({
-
     // root: 'body',
     autoShow: true,
     disablePageInteraction: true,
@@ -13,7 +12,7 @@ CookieConsent.run({
     // revision: 0,
 
     cookie: {
-        name: 'cc_cookie',
+        name: 'cc_cookie'
         // domain: location.hostname,
         // path: '/',
         // sameSite: "Lax",
@@ -35,43 +34,43 @@ CookieConsent.run({
         }
     },
 
-    onFirstConsent: ({cookie}) => {
-        console.log('onFirstConsent fired',cookie);
+    onFirstConsent: ({ cookie }) => {
+        console.log('onFirstConsent fired', cookie);
     },
 
-    onConsent: ({cookie}) => {
-        console.log('onConsent fired!', cookie)
+    onConsent: ({ cookie }) => {
+        console.log('onConsent fired!', cookie);
     },
 
-    onChange: ({changedCategories, changedServices}) => {
+    onChange: ({ changedCategories, changedServices }) => {
         console.log('onChange fired!', changedCategories, changedServices);
     },
 
-    onModalReady: ({modalName}) => {
+    onModalReady: ({ modalName }) => {
         console.log('ready:', modalName);
     },
 
-    onModalShow: ({modalName}) => {
+    onModalShow: ({ modalName }) => {
         console.log('visible:', modalName);
     },
 
-    onModalHide: ({modalName}) => {
+    onModalHide: ({ modalName }) => {
         console.log('hidden:', modalName);
     },
 
     categories: {
         necessary: {
-            enabled: true,  // this category is enabled by default
-            readOnly: true  // this category cannot be disabled
+            enabled: true, // this category is enabled by default
+            readOnly: true // this category cannot be disabled
         },
         analytics: {
             autoClear: {
                 cookies: [
                     {
-                        name: /^_ga/,   // regex: match all cookies starting with '_ga'
+                        name: /^_ga/ // regex: match all cookies starting with '_ga'
                     },
                     {
-                        name: '_gid',   // string: exact cookie name
+                        name: '_gid' // string: exact cookie name
                     }
                 ]
             },
@@ -82,12 +81,7 @@ CookieConsent.run({
                     label: 'Google Analytics',
                     onAccept: () => {},
                     onReject: () => {}
-                },
-                youtube: {
-                    label: 'Youtube Embed',
-                    onAccept: () => {},
-                    onReject: () => {}
-                },
+                }
             }
         },
         ads: {}
@@ -99,7 +93,8 @@ CookieConsent.run({
             en: {
                 consentModal: {
                     title: 'We use cookies',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+                    description:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
                     acceptAllBtn: 'Accept all',
                     acceptNecessaryBtn: 'Reject all',
                     showPreferencesBtn: 'Manage Individual preferences',
@@ -107,7 +102,7 @@ CookieConsent.run({
                     footer: `
                         <a href="#path-to-impressum.html" target="_blank">Impressum</a>
                         <a href="#path-to-privacy-policy.html" target="_blank">Privacy Policy</a>
-                    `,
+                    `
                 },
                 preferencesModal: {
                     title: 'Manage cookie preferences',
@@ -119,18 +114,20 @@ CookieConsent.run({
                     sections: [
                         {
                             title: 'Your Privacy Choices',
-                            description: `In this panel you can express some preferences related to the processing of your personal information. You may review and change expressed choices at any time by resurfacing this panel via the provided link. To deny your consent to the specific processing activities described below, switch the toggles to off or use the “Reject all” button and confirm you want to save your choices.`,
+                            description: `In this panel you can express some preferences related to the processing of your personal information. You may review and change expressed choices at any time by resurfacing this panel via the provided link. To deny your consent to the specific processing activities described below, switch the toggles to off or use the “Reject all” button and confirm you want to save your choices.`
                         },
                         {
                             title: 'Strictly Necessary',
-                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+                            description:
+                                'These cookies are essential for the proper functioning of the website and cannot be disabled.',
 
                             //this field will generate a toggle linked to the 'necessary' category
                             linkedCategory: 'necessary'
                         },
                         {
                             title: 'Performance and Analytics',
-                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+                            description:
+                                'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
                             linkedCategory: 'analytics',
                             cookieTable: {
                                 caption: 'Cookie table',
@@ -143,24 +140,26 @@ CookieConsent.run({
                                     {
                                         name: '_ga',
                                         domain: location.hostname,
-                                        desc: 'Description 1',
+                                        desc: 'Description 1'
                                     },
                                     {
                                         name: '_gid',
                                         domain: location.hostname,
-                                        desc: 'Description 2',
+                                        desc: 'Description 2'
                                     }
                                 ]
                             }
                         },
                         {
                             title: 'Targeting and Advertising',
-                            description: 'These cookies are used to make advertising messages more relevant to you and your interests. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.',
-                            linkedCategory: 'ads',
+                            description:
+                                'These cookies are used to make advertising messages more relevant to you and your interests. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.',
+                            linkedCategory: 'ads'
                         },
                         {
                             title: 'More information',
-                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
+                            description:
+                                'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
                         }
                     ]
                 }
