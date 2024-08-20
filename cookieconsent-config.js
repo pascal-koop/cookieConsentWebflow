1c8c0addeae282f9b5ff59821becceafa9c2f075
Pascal <pascale.koop@live.com>
2024-08-20 12:37:03 +0200

    chore: Update cookie consent configuration to include additional consent options for Google Analytics

--- cookieconsent-config.js
+++ cookieconsent-config.js
@@ -52,6 +52,21 @@ CookieConsent.run({
 
     onModalShow: ({ modalName }) => {
         console.log('visible:', modalName);
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('consent', 'default', {
+            ad_storage: 'denied',
+            analytics_storage: 'denied',
+            ad_user_data: 'denied',
+            ad_personalization: 'denied',
+            personalization_storage: 'denied',
+            functionality_storage: 'granted',
+            security_storage: 'granted',
+            wait_for_update: 500
+        });
+        gtag('set', 'ads_data_redaction', true);
     },
 
     onModalHide: ({ modalName }) => {
@@ -81,6 +96,21 @@ CookieConsent.run({
                     label: 'Google Analytics',
                     onAccept: () => {
                         console.log('ga accepted');
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag() {
+                            dataLayer.push(arguments);
+                        }
+                        gtag('consent', 'default', {
+                            ad_storage: 'denied',
+                            analytics_storage: 'denied',
+                            ad_user_data: 'denied',
+                            ad_personalization: 'denied',
+                            personalization_storage: 'denied',
+                            functionality_storage: 'granted',
+                            security_storage: 'granted',
+                            wait_for_update: 500
+                        });
+                        gtag('set', 'ads_data_redaction', true);
                     },
                     onReject: () => {
                         console.log('ga rejected');
