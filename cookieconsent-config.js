bf11bd5f326ce6bd727b207d429a7ae7ede0cf3b
Pascal <pascale.koop@live.com>
2024-08-22 10:59:46 +0200

    chore: Update cookie consent configuration to include Google Fonts service in necessary category

--- cookieconsent-config.js
+++ cookieconsent-config.js
@@ -61,7 +61,15 @@ CookieConsent.run({
     categories: {
         necessary: {
             enabled: true, // this category is enabled by default
-            readOnly: true // this category cannot be
+            readOnly: true, // this category cannot be
+            services: {
+                // Add services to the necessary category
+                ga: {
+                    label: 'Google Fonts',
+                    description:
+                        'Google Fonts is a library of free licensed font families, an interactive web directory for browsing the library, and APIs for conveniently using the fonts via CSS and Android.'
+                }
+            }
         },
         analytics: {
             autoClear: {
@@ -94,75 +102,6 @@ CookieConsent.run({
     language: {
         default: 'de',
         translations: {
-            en: {
-                consentModal: {
-                    title: 'We use cookies',
-                    description:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
-                    showPreferencesBtn: 'Manage Individual preferences',
-                    // closeIconLabel: 'Reject all and close modal',
-                    footer: `
-                        <a href="#path-to-impressum.html" target="_blank">Impressum</a>
-                        <a href="#path-to-privacy-policy.html" target="_blank">Privacy Policy</a>
-                    `
-                },
-                preferencesModal: {
-                    title: 'Manage cookie preferences',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
-                    savePreferencesBtn: 'Accept current selection',
-                    closeIconLabel: 'Close modal',
-                    serviceCounterLabel: 'Service|Services',
-                    sections: [
-                        {
-                            title: 'Your Privacy Choices',
-                            description: `In this panel you can express some preferences related to the processing of your personal information. You may review and change expressed choices at any time by resurfacing this panel via the provided link. To deny your consent to the specific processing activities described below, switch the toggles to off or use the “Reject all” button and confirm you want to save your choices.`
-                        },
-                        {
-                            title: 'Strictly Necessary',
-                            description:
-                                'These cookies are essential for the proper functioning of the website and cannot be disabled.',
-
-                            //this field will generate a toggle linked to the 'necessary' category
-                            linkedCategory: 'necessary'
-                        },
-                        {
-                            title: 'Performance and Analytics',
-                            description:
-                                'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
-                            linkedCategory: 'analytics',
-                            cookieTable: {
-                                caption: 'Cookie table',
-                                headers: {
-                                    name: 'Cookie',
-                                    domain: 'Domain',
-                                    desc: 'Description'
-                                },
-                                body: [
-                                    {
-                                        name: '_ga',
-                                        domain: location.hostname,
-                                        desc: 'Description 1'
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            title: 'Targeting and Advertising',
-                            description:
-                                'These cookies are used to make advertising messages more relevant to you and your interests. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.',
-                            linkedCategory: 'ads'
-                        },
-                        {
-                            title: 'More information',
-                            description:
-                                'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
-                        }
-                    ]
-                }
-            },
             de: {
                 consentModal: {
                     title: 'Wir verwenden Cookies auf unserer Website',
